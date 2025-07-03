e230f0974966d00bc906f1ab0bdb78ea4f8c9001
seniru-dilmith <dilmithseniru@gmail.com>
2025-07-03 20:14:51 +0530

    improved sitemap and robots generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,14 +6,59 @@ module.exports = {
   changefreq: 'daily',
   priority: 0.7,
   trailingSlash: false,
-  exclude: ['/admin'],
+  outDir: './out',
+  exclude: [
+    '/admin',
+    '/admin/', 
+    '/admin/*', 
+    '/admin/**', 
+    '/admin/dashboard', 
+    '/admin/debug', 
+    '/admin/test', 
+    '/admin/login',
+    '/auth/register', 
+    '/auth/test', 
+    '/checkout', 
+    '/cart'
+  ],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/api'],
       },
     ],
+    additionalSitemaps: [
+      'https://thedreamydelights.com/sitemap.xml',
+    ],
+  },
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: '/',
+        priority: 1.0,
+        changefreq: 'daily',
+      },
+      {
+        loc: '/menu',
+        priority: 0.9,
+        changefreq: 'daily',
+      },
+      {
+        loc: '/about',
+        priority: 0.8,
+        changefreq: 'monthly',
+      },
+      {
+        loc: '/contact',
+        priority: 0.6,
+        changefreq: 'weekly',
+      },
+      {
+        loc: '/auth/login',
+        priority: 0.4,
+        changefreq: 'monthly',
+      },
+    ];
   },
 };
